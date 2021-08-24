d1ac4f87842ee8f79235950b2bd21d3567f29971
Розов Антон <rozov@vologda.softlab.ru>
2021-08-24 07:35:28 +0300

    PllAlgorithmSelection - приведение плашек к единому стилю

--- lib/views/trainers/pll/view/pll_select_view/pll_algorithm_selection_item.dart
+++ lib/views/trainers/pll/view/pll_select_view/pll_algorithm_selection_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:rg2/database/entitys/pll_trainer_item.dart';
+import 'package:rg2/views/shared/menu_card_item.dart';
 import 'package:rg2/views/shared/ui_helpers.dart';
 
 class PllAlgorithmSelectionItem extends StatelessWidget {
@@ -20,49 +21,42 @@ class PllAlgorithmSelectionItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var imagePath = _pllTrainerItem.imagePath;
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: UIHelper.SpaceMini),
-      child: GestureDetector(
-        onTap: () => _onItemTap(_pllTrainerItem),
-        child: Card(
-          elevation: 8.0,
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: UIHelper.SpaceMini, vertical: UIHelper.SpaceMini),
-            child: Row(
-              //mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  child: imagePath.endsWith(".svg") ?
-                    SvgPicture.asset(imagePath, height: 70, width: 70,) :
-                    Image.asset(imagePath, height: 70, width: 70,),
-                ),
-                SizedBox(width: UIHelper.SpaceSmall,),
-                Expanded(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("${_pllTrainerItem.internationalName} ${_pllTrainerItem.maximName}",
-                        style: Get.textTheme.subtitle1.copyWith(fontSize: 14, color: Get.theme.primaryColor),
-                      ),
-                      SizedBox(height: 8.0,),
-                      Text("Название для кнопок:",
-                        style: Get.textTheme.subtitle1.copyWith(fontSize: 14),
-                      ),
-                      SizedBox(height: 8.0,),
-                      Text("${_pllTrainerItem.currentName}",
-                        style: Get.textTheme.subtitle1.copyWith(fontSize: 14, color: Get.theme.accentColor),
-                      )
-                    ],
-                  ),
-                ),
-                Checkbox(
-                  value: _pllTrainerItem.isChecked,
-                  onChanged: (value) => _onChangeCheckBoxValue(value, _pllTrainerItem),
-                )
-              ],
+    return GestureDetector(
+      onTap: () => _onItemTap(_pllTrainerItem),
+      child: MenuCardItem(
+        child: Row(
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              child: imagePath.endsWith(".svg") ?
+                SvgPicture.asset(imagePath, height: 65, width: 65,) :
+                Image.asset(imagePath, height: 65, width: 65,),
             ),
-          ),
+            SizedBox(width: UIHelper.SpaceSmall,),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("${_pllTrainerItem.internationalName} ${_pllTrainerItem.maximName}",
+                    style: Get.textTheme.subtitle1.copyWith(fontSize: 14, color: Get.theme.primaryColor),
+                  ),
+                  SizedBox(height: 8.0,),
+                  Text("Название для кнопок:",
+                    style: Get.textTheme.subtitle1.copyWith(fontSize: 14),
+                  ),
+                  SizedBox(height: 8.0,),
+                  Text("${_pllTrainerItem.currentName}",
+                    style: Get.textTheme.subtitle1.copyWith(fontSize: 14, color: Get.theme.accentColor),
+                  )
+                ],
+              ),
+            ),
+            Checkbox(
+              value: _pllTrainerItem.isChecked,
+              onChanged: (value) => _onChangeCheckBoxValue(value, _pllTrainerItem),
+            )
+          ],
         ),
       ),
     );
